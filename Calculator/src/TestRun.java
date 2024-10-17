452bc052243f5b964ff84c2dce6d1c5b70de550e
tbrkic <minec616@gmail.com>
2024-10-17 11:03:08 +0200

    committing stuff

--- Calculator/src/TestRun.java
+++ Calculator/src/TestRun.java
@@ -11,12 +11,11 @@ public class TestRun {
 		compooper.getCalculator ().setPower ( new PowerOfNumber () );
 		Scanner sc1= new Scanner ( System.in );
 		System.out.println ("This is a calculator app. You may type two numbers of any value followed by a operation(add, sub, div, mul or pow). Butt first, you may decide to stop if you type 'n'."  );
-	switch ( sc1.next (  ) ){
-		case "n":
-			break;
-		default:
-			compooper.doStuff ();
-			System.out.println ("Wanna do it again?" );
-	}
+		if ( sc1.next ( ).equals ( "n" ) ) {
+		}
+		else {
+			compooper.doStuff ( );
+			System.out.println ( "Wanna do it again?" );
+		}
 	}
 }
